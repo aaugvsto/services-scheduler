5a76f281c4b123be34237dbe0519153a78bcf955
Augusto Assis <augustoassis.dev@gmail.com>
2025-07-23 21:18:06 -0300

    :sparkles: feat: worflow tabs for set service, set professional and checkout

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
 app.use(express.static('public'))
+
+app.get("/api/business/data", (req, res) => {
+  //TODO: implement the logic to fetch services from a database or other source
+  
+  const response = {
+    services: [
+      { id: 1, name: "Corte", description: "Corte de cabelo masculino", price: 50.00, currency: "BRL" },
+      { id: 2, name: "Barba", description: "Aparar e modelar a barba", price: 30.00, currency: "BRL" },
+      { id: 3, name: "Corte e Barba", description: "Corte de cabelo e barba", price: 70.00, currency: "BRL" }
+    ],
+    professionals: [
+      { id: 1, name: "João" },
+      { id: 2, name: "Maria" },
+      { id: 3, name: "Pedro" }
+    ]
+  }
+
+  res.json(response)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,43 @@
-var divSelecionarBarbeiro = "#selecionar-barbeiro"
-var divSelecionarServico = "#selecionar-servico"
+var divProfessionalsTabContent = "#professional-tab-content"
+var divServicesTabContent= "#services-tab-content"
+var divCheckoutTabContent = "#checkout-tab-content"
+var divServicesContainer = "#service-list-container"
+var divProfessionalContainer = "#professionals-container"
 var divTituloCard = "#tituloCard"
 var divHorariosBarbeiros = "#horariosBarbeiro"
 var divMsgSelecioneUmBarbeiro = "#msgSelecioneBarbeiro"
+var divTabService = "#tab-services"
+var divTabProfessionals = "#tab-professionals"
+var divTabCheckout = "#tab-checkout"
 
 $(function(){
     init()
+    loadContent()
 })
 
 const init = () => {
-    $(divTituloCard).html('Escolha seu serviço')
-
-    $(divSelecionarServico).show()
-    $(divSelecionarBarbeiro).hide()
-    $(divHorariosBarbeiros).hide()
+    handlerSelectTab(1);
 
     $('#exampleInputPassword1').mask('(00) 00000-0000')
 
     resetProfessionalBorder()
 }
 
-const selecionarServico = () => {
-    $(divTituloCard).html('Escolha seu professional')
-    $(divSelecionarServico).hide()
-    $(divSelecionarBarbeiro).show()
-    $(divHorariosBarbeiros).hide()
-    $(divMsgSelecioneUmBarbeiro).show()
+const setService = (id) => {
+    handlerSelectTab(2)
+    handlerUnselectTab(1)
+
+    localStorage.setItem('serviceId', id)
 }
 
-const selectProfessional = (element) => {
+const selectProfessional = (element, id) => {
     resetProfessionalBorder();
     setProfessional(element)
 
     $(divHorariosBarbeiros).show()
     $(divMsgSelecioneUmBarbeiro).hide()
+
+    localStorage.setItem('setProfessionalId', id)
 }
 
 const setProfessional = (element) => {
@@ -41,6 +45,117 @@ const setProfessional = (element) => {
     $(element).addClass('border-2')
 }
 
+const setHorario = () => {
+    handlerSelectTab(3)
+    handlerUnselectTab(2)
+}
+
 const resetProfessionalBorder = () => {
     $('.border-dark').removeClass('border-dark');
+}
+
+const loadContent = () => {
+    $.get("/api/business/data", (res) => { 
+        const services = res.services;
+        const professionals = res.professionals;
+        
+        services.forEach(service => {
+            $(divServicesContainer).append(`
+                <div class="row">
+                    <div class="col-2 d-flex justify-content-center align-items-center">
+                        <img src="corte.jpg" height="64" width="64" class="rounded">
+                    </div>
+                    <div class="col-7 d-flex flex-column">
+                        <div>
+                            <strong>${service.name}</strong> - 30 min
+                        </div>
+
+                        <div class="scrollable-text">
+                            ${service.description}
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex flex-column justify-content-center align-items-center">
+                        <div>
+                            <h6>${Number(service.price).toLocaleString('pt-BR', {
+                                style: 'currency', 
+                                currency: service.currency
+                            })}</h6>
+                        </div>
+                        <div>
+                            <button class="btn btn-outline-dark" onclick="setService(${service.id})">Agendar</button>
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+            `)
+        });
+
+        professionals.forEach(professional => {
+            $(divProfessionalContainer).append(`
+                <div class="me-1" style="min-width: 80px; cursor: pointer;">
+                    <div class="d-flex flex-column border rounded p-2" onclick="selectProfessional(this, ${professional.id})">
+                        <div>
+                            <img src="corte.jpg" height="64" width="64" class="rounded-circle">
+                        </div>
+                        <div class="w-100 text-center">
+                            <span class="fw-bold">${professional.name}</span>
+                        </div>
+                    </div>
+                </div>
+            `)
+        })
+    })
+}
+
+const handlerSelectTab = (tabId) => {
+    let div = getDivTab(tabId)
+    
+    $(div).removeClass('bg-dark')
+    $(div).addClass('bg-light')
+    $(div).addClass('text-dark')
+    $(div).removeClass('text-light')
+
+    switch(tabId) {
+        case 1:
+            $(divServicesTabContent).show()
+            $(divProfessionalsTabContent).hide()
+            $(divHorariosBarbeiros).hide()
+            $(divCheckoutTabContent).hide()
+            break;
+        case 2:
+            $(divServicesTabContent).hide()
+            $(divProfessionalsTabContent).show()
+            $(divHorariosBarbeiros).hide()
+            $(divCheckoutTabContent).hide()
+            break;
+        case 3:
+            $(divServicesTabContent).hide()
+            $(divProfessionalsTabContent).hide()
+            $(divHorariosBarbeiros).hide()
+            $(divMsgSelecioneUmBarbeiro).hide()
+            $(divCheckoutTabContent).show()
+            break;
+    }
+}
+
+const handlerUnselectTab = (tabId) => {
+    let div = getDivTab(tabId)
+
+    $(div).addClass('bg-dark')
+    $(div).removeClass('bg-light')
+    $(div).removeClass('text-dark')
+    $(div).addClass('text-light')
+}
+
+const getDivTab = (tabId) => {
+    switch(tabId) {
+        case 1:
+            return divTabService;
+        case 2:
+            return divTabProfessionals;
+        case 3:
+            return divTabCheckout;
+        default:
+            return '';
+    }
 }
